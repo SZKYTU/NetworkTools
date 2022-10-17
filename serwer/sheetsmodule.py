864e3445501ecf52b06522df9080fd065d382233
SZKYTU-mac <szkycode@gmail.com>
2022-10-17 22:42:58 +0200

    sheetsaddwork

--- serwer/sheetsmodule.py
+++ serwer/sheetsmodule.py
@@ -1,6 +1,12 @@
 import gspread
 import sys
-sys.path.insert(1, 'C:/Users/czupyk_p/Documents/Python/NetworkTools') # FIX THIS BUUBLE
+import os
+# sys.path.insert(1, 'C:/Users/czupyk_p/Documents/Python/NetworkTools') # FIX THIS BUUBLE
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
 from main import UserInfo
 
 sa = gspread.service_account(filename="googlekey.json")
@@ -8,4 +14,4 @@ sh = sa.open("NetworkTools")
 
 wks = sh.worksheet("IPdb")
 
-wks.update("A", UserInfo.getIP())
+wks.append_row([UserInfo.getIP()])
