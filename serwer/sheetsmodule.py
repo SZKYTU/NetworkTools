72badfb217682b565bb9cbad732d7d865fb064fb
Szkytu_Work <szkycode@gmail.com>
2022-10-17 14:25:36 +0200

    writeTestDataWork!

--- serwer/sheetsmodule.py
+++ serwer/sheetsmodule.py
@@ -1,8 +1,11 @@
 import gspread
+import sys
+sys.path.insert(1, 'C:/Users/czupyk_p/Documents/Python/NetworkTools') # FIX THIS BUUBLE
+from main import UserInfo
 
 sa = gspread.service_account(filename="googlekey.json")
 sh = sa.open("NetworkTools")
 
 wks = sh.worksheet("IPdb")
 
-wks.update("A2", "TEST")
+wks.update("A", UserInfo.getIP())
