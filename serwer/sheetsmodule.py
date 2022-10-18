dfe7c79854a7d63e6a376d59ddae370a22e666d1
Szkytu_Work <szkycode@gmail.com>
2022-10-18 09:24:00 +0200

    sheetsModuleWork!

--- serwer/sheetsmodule.py
+++ serwer/sheetsmodule.py
@@ -1,17 +1,21 @@
+from datetime import date
 import gspread
 import sys
 import os
-# sys.path.insert(1, 'C:/Users/czupyk_p/Documents/Python/NetworkTools') # FIX THIS BUUBLE
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+today = date.today()
 
 from main import UserInfo
 
-sa = gspread.service_account(filename="googlekey.json")
+sa = gspread.service_account(filename="googlekey2.json")
 sh = sa.open("NetworkTools")
 
 wks = sh.worksheet("IPdb")
 
-wks.append_row([UserInfo.getIP()])
+wks.append_row([UserInfo.getIP(), 
+                UserInfo.getMAC(),
+                UserInfo.getHostname(),
+                str(today.strftime("%d/%m/%Y"))])
