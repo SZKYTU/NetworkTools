2ce4dd12fa98c2b9cf218c1986d0848ef4039fd8
Szkytu_Work <szkycode@gmail.com>
2022-10-21 13:47:29 +0200

    socketWork!

--- serwer/socketserwer.py
+++ serwer/socketserwer.py
@@ -1,6 +1,16 @@
+from base64 import decode
+from email.header import Header
+from http import client
+import json
 import socket 
-from config import socketHost,socketPort
+import os
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 
+BUFFER = 1024
+from config import socketHost,socketPort
 
 socket = socket.socket(socket.AF_INET  , socket.SOCK_STREAM)
 socket.bind((socketHost, socketPort))
@@ -8,4 +18,6 @@ socket.listen(2)
 
 while True:
     cliSocket, adress = socket.accept()
-    print(f'Connect {adress}')
+    print(cliSocket)
+    get = cliSocket.recv(BUFFER).decode("utf8")
+    print(get)
--- socketclient.py
+++ socketclient.py
@@ -1,8 +1,22 @@
+from http import client
+from re import S
 import socket
+import json
 from config import socketHost,socketPort
+from main import UserInfo
 
 
+UserInfoJS = {
+    "IP": UserInfo.getIP(),
+    "Hostname": UserInfo.getHostname(),
+    "MAC": UserInfo.getMAC()}
+
+json = json.dumps(UserInfoJS)
+
+    
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((socketHost, socketPort))
 
+json = json.encode("utf8")
 
+socket.sendall(json)
