9fc9d8ed98315719ddd22632d97a5f1a25e5310a
Szkytu_Work <szkycode@gmail.com>
2022-10-25 07:49:23 +0200

    propably all work ;)

--- serwer/sheetsmodule.py
+++ serwer/sheetsmodule.py
@@ -3,20 +3,15 @@ import sys
 import json
 import gspread
 from datetime import date
-from socketserwer import get #<-- right data from serwer 
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
-today = date.today()
 
 def sheetInsert(json):
-    sa = gspread.service_account(filename="googlekey2.json")
+    sa = gspread.service_account(filename="googlekey.json")
     sh = sa.open("NetworkTools")
 
     wks = sh.worksheet("IPdb")
-    wks.append_row([get['IP'], 
-                    # UserInfo.getMAC(),
-                    # UserInfo.getHostname(),
-                    # str(today.strftime("%d/%m/%Y"))
+    wks.append_row([json['IP'],
+                    json['MAC'],
+                    json['Hostname'],
+                    str(date.today().strftime("%d/%m/%Y"))
                     ]
-                    )
+                   )
--- serwer/socketserwer.py
+++ serwer/socketserwer.py
@@ -1,33 +1,11 @@
-from datetime import date
-import gspread
-from base64 import decode
 import os
 import sys
 import json
 import socket 
-from http import client
-from email.header import Header
+from sheetsmodule import sheetInsert
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
-
-today = date.today()
-
-
-def sheetInsert(json):
-    sa = gspread.service_account(filename="googlekey.json")
-    sh = sa.open("NetworkTools")
-
-    wks = sh.worksheet("IPdb")
-    wks.append_row([json['IP'],
-                    json['MAC'],
-                    json['Hostname'],
-                    str(today.strftime("%d/%m/%Y"))
-                    ]
-                   )
-
-
-
 
 
 BUFFER = 1024
